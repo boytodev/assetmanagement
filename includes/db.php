76573c600db8580b7d6abb94a183265408e3e7e3
boytodev <boydevpro@gmail.com>
2025-05-18 11:46:05 +0700

    dev

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,12 @@
 <!-- /includes/db.php -->
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = 'boy191147';
-$dbname = 'assetmanagement';
+$host = 'sql103.infinityfree.com';
+$user = 'if0_39011339';
+$pass = 'boy9876543';
+$dbname = 'if0_39011339_db_equipment';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
+$conn->set_charset("utf8mb4");
 if ($conn->connect_error) {
     die("เชื่อมต่อฐานข้อมูลล้มเหลว: " . $conn->connect_error);
 }
